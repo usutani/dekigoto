0774e01ce74be42848da65e8596ac1af175c8e72
Yasuhiro Usutani <y_usutani@mac.com>
2026-05-06 14:37:01 +0900

    r g migration add_ends_at_to_events ends_at:datetime

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_05_06_051946) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_06_053503) do
   create_table "events", force: :cascade do |t|
     t.datetime "created_at", null: false
+    t.datetime "ends_at"
     t.string "name", null: false
     t.datetime "starts_at", null: false
     t.datetime "updated_at", null: false
