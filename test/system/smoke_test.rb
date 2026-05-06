2ebf1722ef80485021f93a349f588951efd16dc7
Yasuhiro Usutani <y_usutani@mac.com>
2026-05-06 15:41:14 +0900

    Add system test

---
+++ test/system/smoke_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class SmokesTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit smokes_url
+  #
+  #   assert_selector "h1", text: "Smoke"
+  # end
+end
